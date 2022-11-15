d0401c581766bd3be31f2e76f66d3d91e362ec06
kartik-99-ux <kartikpandeyofficial@gmail.com>
2022-11-16 00:06:14 +0530

    Question_4 commit

---
+++ Question_4.java
@@ -0,0 +1,24 @@
+
+public class Question_4 {
+
+	public static void main(String[] args) 
+	{   int n=30;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n-1 || i==n-2 || j==0 && i>=n/2 || j==n-1 && i>=n/2 || i-j<=n-2  && i-j>=n/2 && i>=n/2 && i<n-2 && j<n/2  || i>=n/2 && j>n/2 && i+j>=n+(n-1)/2 && i<n-2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
